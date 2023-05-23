cd473e53f9bf49a3c861505e3e9c2af73442bfe4
David McManamon <dmcmanam@gmail.com>
2023-05-23 07:56:51 -0400

    Create rsync_promethion.sh

---
+++ archiving/rsync_promethion.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Copy runs off the Oxford Nanopore Promethion, only allow one rsync to run on the machine at a time
+
+ARCHFILE=COPIED
+SOURCE=/data
+DEST=igo@igo-ln01:/igo/staging/promethion
+LOCK=/home/igo/.md5locks/rsync.lock
+RSYNC="rsync -a --no-links --exclude=nohup.out "
+
+if [ -f $LOCK ]
+ then
+  echo "Per $LOCK, $0 is already running. `date`"
+  exit 2
+fi
+
+touch $LOCK
+echo
+echo "Starting  $0. `date`"
+
+function rsync_run {
+    RUN=$1
+    #chmod +x $SOURCE/$RUN
+
+    echo "Archiving $SOURCE/$RUN `date` from $SOURCE/$RUN to $DEST/$RUN"
+    $RSYNC --exclude=$ARCHFILE $SOURCE/$RUN $DEST/$RUN
+    if [ $? != 0 ]
+     then
+      echo "$0: rsync #1 error on $RUN"
+      echo "Failed command: $RSYNC $SOURCE/$RUN $DEST/$RUN"
+      echo "Failed command: $RSYNC $SOURCE/$RUN $DEST/$RUN" | mail -s "$0: rsync #1 error on $RUN" mcmanamd@mskcc.org
+    fi # [ $? != 0 ]
+
+    date >> $RUN$ARCHFILE
+    # Copy again, and this time include the new marker file
+    $RSYNC -v  $SOURCE/$RUN $DEST/$RUN
+    if [ $? != 0 ]
+     then
+      echo "$0: rsync #2 error on $RUN"
+      echo "Failed command: $RSYNC -v                                       $SOURCE/$RUN $DEST/$RUN"
+      echo "Failed command: $RSYNC -v                                       $SOURCE/$RUN $DEST/$RUN" | mail -s "$0: rsync #2 error on $RUN" mcmanamd@mskcc.org
+    fi # [ $? != 0 ]
+
+    echo "Archived $SOURCE/$RUN to $DEST/$RUN `date`"
+}
+
+cd $SOURCE
+RUNS=`ls -d Project_*/`
+for RUN in $RUNS
+ do
+  if [ ! -e $RUN$ARCHFILE ]
+   then
+    rsync_run $RUN &
+  fi 
+ done
+
+wait
+rm $LOCK
+echo "Completed $0. `date`"
