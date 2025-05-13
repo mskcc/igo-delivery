297b4addf82ac23aad9f487028c7eea5b1f0acfb
David McManamon <dmcmanam@gmail.com>
2025-05-13 07:58:31 -0400

    Update fastq-md5-hash-b.sh

--- archiving/fastq-md5-hash-b.sh
+++ archiving/fastq-md5-hash-b.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+# Generate md5 hash for all fastq.gz files on a run
 
-MODE=archive
 RUNS=/igo/delivery/FASTQ/*/
 LOCK=/home/seqdataown/.md5locks/FASTQ_HASHING_ARCHIVE
-READYFILE=ARCHIVED
+READYFILE=ARCHIVED_AT_IGO
 
 if [ -f $LOCK ]
  then
@@ -18,20 +18,17 @@ echo "Starting  $0. `date`"
 for RUN in $RUNS
  do
   cd $RUN
-   if [ -e $READYFILE -a ! -e fastq.md5.$MODE ]
+   if [ -e $READYFILE -a ! -e fastq.md5.archive ]
     then
-     echo "Generating $RUN fastq.md5.$MODE. `date`"
-     find . -name \*\.fastq.gz -print0 | xargs -0 -n1 -P20 md5sum --tag | sort > fastq.md5.$MODE.partial
-     mv fastq.md5.$MODE.partial fastq.md5.$MODE
-     echo "Generated  $RUN fastq.md5.$MODE. `date`"
-
-     if [ $MODE = archive ]
-      then
-      RUNNAME=$(basename $RUN)
-       curl "http://igodb.mskcc.org:8080/ngs-stats/rundone/fastq/$RUNNAME" &
-       (diff -qs fastq.md5.input fastq.md5.archive; wc -l fastq.md5.*) | mail -s "Hashed and archived: $RUN" mcmanamd@mskcc.org,naborsd@mskcc.org,luc@mskcc.org
-     fi # [ $MODE = archive ]
-   fi # [ -e $READYFILE -a ! -e $TYPE.md5.$MODE ]
+     echo "Generating $RUN fastq.md5.archive. `date`"
+     find . -name \*\.fastq.gz -print0 | xargs -0 -n1 -P20 md5sum --tag | sort > fastq.md5.archive.partial
+     mv fastq.md5.archive.partial fastq.md5.archive
+     echo "Generated  $RUN fastq.md5.archive. `date`"
+     
+     RUNNAME=$(basename $RUN)
+     curl "http://igodb.mskcc.org:8080/ngs-stats/rundone/fastq/$RUNNAME" &
+     (diff -qs fastq.md5.input fastq.md5.archive; wc -l fastq.md5.*) | mail -s "Hashed and archived: $RUN" mcmanamd@mskcc.org,luc@mskcc.org,naborsd@mskcc.org
+   fi # [ -e $READYFILE -a ! -e $TYPE.md5.archive ]
  done
 
 rm $LOCK
