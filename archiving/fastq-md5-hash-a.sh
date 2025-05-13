c43e1d2272cc0638fe987ae5de0d5fd8b9e5cd24
David McManamon <dmcmanam@gmail.com>
2025-05-13 07:56:26 -0400

    Update fastq-md5-hash-a.sh

--- archiving/fastq-md5-hash-a.sh
+++ archiving/fastq-md5-hash-a.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 MODE=input
-RUNS=/igo/sequencers/FASTQ/*/
-LOCK=/home/seqdataown/.locks/FASTQ_HASHING_INPUT_NJ
+RUNS=/igo/staging/FASTQ/*/
+LOCK=/home/seqdataown/.md5locks/FASTQ_HASHING_INPUT
 READYFILE=RTAComplete.txt
 
 if [ -f $LOCK ]
@@ -21,7 +21,7 @@ for RUN in $RUNS
    if [ -e $READYFILE -a ! -e fastq.md5.$MODE ]
     then
      echo "Generating $RUN fastq.md5.$MODE. `date`"
-     find . -name \*\.fastq.gz -print0 | xargs -0 -n1 -P20 md5sum --tag | sort > fastq.md5.$MODE.partial
+     find . -name \*\.fastq.gz ! -name '*Undetermined_*' -print0 | xargs -0 -n1 -P20 md5sum --tag | sort > fastq.md5.$MODE.partial
      mv fastq.md5.$MODE.partial fastq.md5.$MODE
      echo "Generated  $RUN fastq.md5.$MODE. `date`"
    fi # [ -e $READYFILE -a ! -e $TYPE.md5.$MODE ]
